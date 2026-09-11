dfbe15fddba63941271af49e7144f74e31386a34
zahrandrf-maker <zahran.dhiarf@gmail.com>
2026-09-12 01:00:40 +0700

    chore: add src/BRBrowserSource.h

---
+++ src/BRBrowserSource.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+
+#include <ffgl/FFGLPluginSDK.h>
+#include <ffgl/FFGLQuad.h>
+#include <ffgl/FFGLShader.h>
+
+class BRBrowserSource final : public CFFGLPlugin
+{
+public:
+    BRBrowserSource();
+    ~BRBrowserSource() override;
+
+    FFResult InitGL(const FFGLViewportStruct* viewport) override;
+    FFResult ProcessOpenGL(ProcessOpenGLStruct* processStruct) override;
+    FFResult DeInitGL() override;
+    FFResult SetFloatParameter(unsigned int index, float value) override;
+    float GetFloatParameter(unsigned int index) override;
+    FFResult SetTextParameter(unsigned int index, const char* value) override;
+    char* GetTextParameter(unsigned int index) override;
+    char* GetParameterDisplay(unsigned int index) override;
+
+private:
+    CFFGLShader shader;
+    CFFGLQuad quad;
+    GLint accentLocation = -1;
+    std::string browserUrl = "https://vdo.ninja/";
+    std::string status = "Browser renderer: pending";
+    bool captureEnabled = false;
+    bool showCursor = true;
+    bool audioEnabled = true;
+    bool refreshRequested = false;
+    float fpsSelector = 0.0f;
+};
